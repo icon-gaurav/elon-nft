1b4c2ec48519c0c0d41bed6c479c18b24cbea13d
gaurav <icon.gaurav806@gmail.com>
2022-08-29 19:48:22 +0530

    elon nft minted

--- scripts/elonnft.js
+++ scripts/elonnft.js
@@ -8,6 +8,9 @@ async function main() {
     await elon.deployed();
 
     console.log("ElonNFT deployed to:", elon.address);
+
+    let txn = await elon.mintNFT();
+    await txn.wait();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
